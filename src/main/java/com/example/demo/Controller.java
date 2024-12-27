82186c8c76c387411792ea81528bf34c1849edb2
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2024-12-26 20:24:33 -0700

    Add rough routes

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,13 +1,51 @@
 package com.example.demo;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class Controller {
 
+    private Map<String, Device> db = new HashMap<>() {{
+        put("1", new Device("1", "HVAC"));
+    }};
+
     @GetMapping("/")
-    public String hello() {
+    public String mainPage() {
+
         return "Hello World!";
+    }
+
+    @GetMapping("/getDevices")
+    public Collection<Device> getDevices() {
+        return db.values();
+    }
+
+    @GetMapping("/getDevice/{id}")
+    public String getDevice(@PathVariable @Valid String id) {
+        return "Find device " + id;
+    }
+
+
+    @GetMapping("/command/{id}/{action}")
+    public String commandDevice(@PathVariable @Valid String id, @PathVariable @Valid String action) {
+
+        return "Device: " + id + ", Command: " + action;
+    }
+
+    @GetMapping("/stop/{id}")
+    public String stopDevice(@PathVariable @Valid String id) {
+        Device device = db.get(id);
+        if (device == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return "Device " + id + " Stopped";
     }
 }
---
+++ src/main/java/com/example/demo/Device.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+public class Device {
+
+    private String id;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public Device(String id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+}
